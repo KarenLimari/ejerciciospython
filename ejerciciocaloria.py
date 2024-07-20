8475e98a5111ca02bc30e0acb780c38a8d3a61d1
Karen Limari <karenlimari20@gmail.com>
2024-07-20 02:01:18 +0200

    feat:ejerciciospython

---
+++ ejerciciocaloria.py
@@ -0,0 +1,14 @@
+import math
+
+
+
+#variables a utilizar
+proteina = float(input("Ingrese los gr de proteina:\n>"))
+carbohidratos = float(input("Ingrese los gr de carbohidratos:\n>"))
+grasa = float(input("Ingrese los gr de grasa:\n>"))
+
+calorias = 4 * (proteina + carbohidratos) * 9 * grasa
+calorias = math.ceil(calorias)
+
+print(calorias)
+print (f"Las calorias totales del producto son: {calorias}")
