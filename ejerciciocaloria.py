2a50a83ad1462dec575eb04ebacdf83c3b296645
Karen Limari <karenlimari20@gmail.com>
2024-07-20 02:09:18 +0200

    feat:ejerciciospython-formulaok

--- ejerciciocaloria.py
+++ ejerciciocaloria.py
@@ -7,8 +7,8 @@ proteina = float(input("Ingrese los gr de proteina:\n>"))
 carbohidratos = float(input("Ingrese los gr de carbohidratos:\n>"))
 grasa = float(input("Ingrese los gr de grasa:\n>"))
 
-calorias = 4 * (proteina + carbohidratos) * 9 * grasa
-calorias = math.ceil(calorias)
+calorias = 4 * (proteina + carbohidratos) + 9 * grasa
+calorias = math.ceil(calorias) #operacion separada del print
 
 print(calorias)
 print (f"Las calorias totales del producto son: {calorias}")
